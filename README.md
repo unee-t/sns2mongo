2232f2cc1c424909e4d010b4b4a7ac08f1f1f5fc
Kai Hendry <hendry@webconverger.com>
2018-05-30 15:48:24 +0800

    Partly addresses https://github.com/unee-t/frontend/issues/244

---
+++ README.md
@@ -0,0 +1,10 @@
+Part of the [notification project](https://github.com/orgs/unee-t/projects/2)
+
+This inserts the SNS message payload from **ut_notification_messages_cases** to
+**CaseNotifications** in the meteor DB.
+
+Next [the Frontend](https://github.com/unee-t/frontend), using Meteor should be
+able to present a notification to the user by observing changes in the
+**CaseNotifications** collection.
+
+<video controls src=https://s.natalian.org/2018-05-30/sns2mongo.mp4></video>
--- functions/simple/index.js
+++ functions/simple/index.js
@@ -11,17 +11,55 @@ var validate = validator({
     update_what: { required: true, type: 'string' }
   }
 })
-exports.handle = function (e, ctx, cb) {
-  console.log('processing event: %j', e.Records[0])
-  console.log('processing message: %j', e.Records[0].Sns.Message)
-  var payload = JSON.parse(e.Records[0].Sns.Message)
-  console.log('payload', payload)
+
+const AWS = require('aws-sdk')
+const ssm = new AWS.SSM()
+
+const getParam = async name => {
+  const data = await ssm
+    .getParameter({
+      Name: name,
+      WithDecryption: true
+    })
+    .promise()
+
+  return data.Parameter.Value
+}
+
+console.log('Starting up')
+
+exports.handle = async function (e, ctx, cb) {
+  const payload = JSON.parse(e.Records[0].Sns.Message)
   if (validate(payload)) {
-    console.log('should be valid')
+    const MongoClient = require('mongodb').MongoClient
+
+    // Connection URL
+
+    const pass = await getParam('MONGO_PASSWORD')
+    const connect = await getParam('MONGO_CONNECT')
+    const url = `mongodb://root:${pass}@${connect}`
+
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function (err, client) {
+      if (err != null) {
+        cb(err)
+      }
+      console.log('Connected successfully to server')
+
+      const db = client.db('meteor')
+
+      payload.created_datetime = new Date(payload.created_datetime + ' UTC')
+
+      db.collection('CaseNotifications').insertOne(payload, function (err, r) {
+        client.close()
+        cb(err)
+      })
+    })
   } else {
     console.log('oh no, invalid')
     console.log(validate.errors)
   }
 
+  console.log('All done')
   cb(null)
 }
